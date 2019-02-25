36e460d3f9f6f2bb030c6b61fbfce51b978d9509
MaestroShifu <sdanielsarsantos@gmail.com>
2019-02-25 00:59:37 -0500

    Se sube correccion de protocolos https en deploy firebase

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -11,10 +11,8 @@ export class ServiceService {
   private host: string;
   private key: string;
 
-  // https://gateway.marvel.com:443/v1/public/characters?limit=10&offset=0&apikey=bb176649dd28fc5a09bf67e88a6b326f
-
   constructor(private http: HttpClient) {
-    this.host = 'http://gateway.marvel.com/v1/public';
+    this.host = 'https://gateway.marvel.com/v1/public';
     this.key = 'ts=1&apikey=bb176649dd28fc5a09bf67e88a6b326f&hash=d69b04813f18aecc036b69c34d0c6fef'
   }
 
